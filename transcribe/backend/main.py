4038500a72f8d4ce5fe59ccfcf38a0f0f7158712
Julie <jihyeo2@illinois.edu>
2020-04-10 07:45:10 -0500

    created a transcribe directory and descending files

---
+++ transcribe/backend/main.py
@@ -0,0 +1,34 @@
+import flask
+import flask_cors import CORS
+#Python 2.x program to transcribe an Audio file 
+import speech_recognition as sr 
+  
+AUDIO_FILE = ("news.wav") 
+  
+# use the audio file as the audio source 
+  
+r = sr.Recognizer() 
+  
+with sr.AudioFile(AUDIO_FILE) as source: 
+    #reads the audio file. Here we use record instead of 
+    #listen 
+    audio = r.record(source)   
+  
+try: 
+    text=r.recognize_google(audio)
+  
+except sr.UnknownValueError: 
+    text="Google Speech Recognition could not understand audio"
+  
+except sr.RequestError as e: 
+    text="Could not request results from Google Speech Recognition service; {0}".format(e) 
+
+
+app = flask.Flask("__main__")
+CORS(app)
+
+@app.route("/")
+def video():
+    return flask.render_template("index.thml", token=text)
+
+app.run(debug=True)

77da285e07e96e97ec1c9958a21a8dd3ce44b5a5
Julie <jihyeo2@illinois.edu>
2020-04-10 08:39:29 -0500

    added os lib

--- transcribe/backend/main.py
+++ transcribe/backend/main.py
@@ -2,8 +2,15 @@ import flask
 from flask_cors import CORS
 #Python 2.x program to transcribe an Audio file 
 import speech_recognition as sr 
+import os
+
+command2mp3="ffmpeg -i ./../video1.mp4 video1.mp3"
+command2wav="ffmpeg -i video1.mp3 video1.wav"
+
+os.system(command2mp3)
+os.system(command2wav)
   
-AUDIO_FILE = ("news.wav") 
+AUDIO_FILE = ("video1.wav") 
   
 # use the audio file as the audio source 
   

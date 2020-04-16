97168a06bf365b39a775dba8826892d189f9823e
alyssaf5 <60593007+alyssaf5@users.noreply.github.com>
2020-04-15 22:48:14 -0500

    Update magic.js

--- scribr-project/src/components/magic.js
+++ scribr-project/src/components/magic.js
@@ -9,7 +9,9 @@ import ReactPlayer from 'react-player';
 // TODO: Make a loading screen so that user knows the progress of transcription before it goes to magictwo page - Jessica
 
 class Magic extends Component {
-    state = {  }
+    state = { 
+        const value = !this.state.value
+    }
     render() {
         return(
             <div className = "word">

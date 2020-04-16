759324ef3710a34a60462404ce9af78298f3c6e7
alyssaf5 <60593007+alyssaf5@users.noreply.github.com>
2020-04-15 22:15:46 -0500

    Update magic.js

--- scribr-project/src/components/magic.js
+++ scribr-project/src/components/magic.js
@@ -28,7 +28,7 @@ class Magic extends Component {
                         label="URL..."
                         style={{width: '400px'}}
                         input="URL"
-                        if (input: true) {
+                        if (magic-url: true) {
                             value={this.state.value}
                         }
                     />

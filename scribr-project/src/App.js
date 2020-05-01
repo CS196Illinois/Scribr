295285d6bc1f7c7235889d0ba8f2921c99542d95
Julie <jihyeo2@illinois.edu>
2020-05-01 01:29:50 -0500

    completed ui design improvement

--- scribr-project/src/App.js
+++ scribr-project/src/App.js
@@ -2,41 +2,37 @@ import React from 'react';
 import './App.css';
 import {Layout, Navigation, Content, Header, Footer} from 'react-mdl';
 import Logo from './pictures/logo.png';
-import LogoHat from './pictures/logo_hat.png';
 import Main from './components/main';
+
+
+
 function App() {
   return (
     <div>
-    {/* <div style={{height: '800px', position: 'relative'}}> */}
     <Layout fixedHeader>
-        <Header className = "header-color" transparent title={<span style={{ color: '#201115' }}></span>}>
-        <a href="/">
-          <img
-            src={Logo} 
-            alt="logo1"
-            className="scribr-logo"
-          />
-        </a>
-        {/* <img
-           src={LogoHat}
-           alt="logo2"
-           align="middle"
-           className="scribr-hat"
-        /> */}
-        <div className ="navigationbar">
-            <Navigation>
-                <a href="/about" style={{color: 'black'}}><font size="4">About</font></a>
-                <a href="/magic" style={{color: 'black'}}><font size="4">Transcribe</font></a>
-                <a href="/people" style={{color: 'black'}}><font size="4">Team</font></a>
-            </Navigation>
+      <div className="header">
+          <Header className = "header-color" transparent title={<span style={{ color: '#201115' }}></span>}>
+          <a href="/">
+            <img
+              src={Logo} 
+              alt="logo1"
+              className="scribr-logo"
+            />
+          </a>
+        
+          <div className ="navigationbar">
+              <Navigation>
+                  <a href="/about" style={{color: 'black'}}><font size="4">About</font></a>
+                  <a href="/magic" style={{color: 'black'}}><font size="4">Transcribe</font></a>
+                  <a href="/people" style={{color: 'black'}}><font size="4">Team</font></a>
+              </Navigation>
+          </div>
+          </Header>
         </div>
-        </Header>
-        <br/>
         <Content>
           <div className="page-content"/>
           <Main />
         </Content>
-        
     </Layout>
 </div>
   );
--- scribr-project/src/components/landingpage.js
+++ scribr-project/src/components/landingpage.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import {TransitionGroup} from 'react-transition-group';
-import name from '../pictures/scribr-name.png';
-import walking from '../pictures/full run(6).gif'
+import walking from '../pictures/full run(i).gif'
 import { Button } from '@material-ui/core';
+import Fade from 'react-reveal/Fade';
+import Flip from 'react-reveal/Flip';
 import './landingpage.css';
 
 const style = {
@@ -21,29 +21,38 @@ class Landing extends Component {
         return(
         <div className = "landingLogo">
             <div className="landing-header">
-                <h2>Make your life easier</h2>
-                <h2>with</h2>
+                <Fade bottom>
+                    <h2>Make your life easier</h2>
+                </Fade>
+                <Fade bottom>
+                    <h2>with</h2>
+                </Fade>
                 <div className="landing-body">
                     <img className="barimage" src={walking} alt="barimage"/>
                 </div>
-                <h1>SCRIBR</h1>
+                <Flip bottom delay={3800}>
+                    <h1>SCRIBR</h1>
+                </Flip>
+                
                 <div className="middle-header">
                     <div className="buttons">
+                        
                         <div className="bsss">
-                            <div className="spot spot1">
-                                <Button className="bs about" variant="contained" color="primary" size="large"><a href="/about" style={{color: 'white'}}><font size="4">About</font></a></Button>
-                            </div>
-                            <div className="spot spot2">
-                                <Button className="bs start" variant="contained" color="secondary" size="large"><a href="/magic" style={{color: 'white'}}><font size="4">Get Started</font></a></Button>
-                            </div>
-                            <div className="spot spot3">
-                                <Button className="bs team" variant="contained" color="#00ff73" size="large"><a href="/people" style={{color: 'white'}}><font size="4">Team</font></a></Button>
-                            </div>
+                            <Fade bottom delay= {4800}>
+                                <div className="spot spot1">
+                                    <Button className="bs about" variant="contained" color="primary" size="large"><a href="/about" style={{color: 'white'}}><font size="4">About</font></a></Button>
+                                </div>
+                                <div className="spot spot2">
+                                    <Button className="bs start" variant="contained" color="secondary" size="large"><a href="/magic" style={{color: 'white'}}><font size="4">Get Started</font></a></Button>
+                                </div>
+                                <div className="spot spot3">
+                                    <Button className="bs team" variant="contained" color="#00ff73" size="large"><a href="/people" style={{color: 'white'}}><font size="4">Team</font></a></Button>
+                                </div>
+                            </Fade>
                         </div>
+                        
+                        
                     </div>
-                    {/* <div className="name">
-                        <img className="nameimg" src={name} alt="nameimg"/>
-                    </div> */}
                 </div>
             </div>
         </div>

8c8ca6e638861a33779e4112277c9b547f599656
jihyeo2 <55719339+jihyeo2@users.noreply.github.com>
2020-05-01 02:01:37 -0500

    Update main.js

--- scribr-project/src/components/main.js
+++ scribr-project/src/components/main.js
@@ -4,7 +4,6 @@ import {Switch, Route, BrowserRouter} from 'react-router-dom';
 import About from './about';
 import People from './people';
 import Magic from './magic';
-import MagicTwo from './magictwo';
 
 const Main = () => (
    <BrowserRouter>
@@ -13,7 +12,6 @@ const Main = () => (
       <Route path="/about" component={About} />
       <Route path="/people" component={People} />
       <Route path="/magic" component={Magic} />
-      <Route path="/magictwo" component={MagicTwo} />
     </Switch> 
    </BrowserRouter>
 )

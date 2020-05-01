3ce5dea58ffc9aa4a8864d4aa26a532932e5af32
jihyeo2 <55719339+jihyeo2@users.noreply.github.com>
2020-05-01 01:53:28 -0500

    Revert "Revert "Jihyeo2""

--- scribr-project/src/App.js
+++ scribr-project/src/App.js
@@ -1,36 +1,34 @@
 import React from 'react';
 import './App.css';
-import {Layout, Navigation, Content, Header} from 'react-mdl';
-import Logo from './pictures/name.png';
-import LogoHat from './pictures/logo_hat.png';
+import {Layout, Navigation, Content, Header, Footer} from 'react-mdl';
+import Logo from './pictures/logo.png';
 import Main from './components/main';
+
+
+
 function App() {
   return (
-    <div style={{height: '800px', position: 'relative'}}>
+    <div>
     <Layout fixedHeader>
-        <Header className = "header-color" transparent title={<span style={{ color: '#201115' }}></span>}>
-        <a href="/">
-          <img
-            src={Logo} 
-            alt="logo1"
-            className="scribr-logo"
-          />
-        </a>
-        <img
-           src={LogoHat}
-           alt="logo2"
-           align="middle"
-           className="scribr-hat"
-        />
-        <div className ="navigationbar">
-            <Navigation>
-                <a href="/about" style={{color: 'black'}}><font size="4">About</font></a>
-                <a href="/magic" style={{color: 'black'}}><font size="4">Magic</font></a>
-                <a href="/people" style={{color: 'black'}}><font size="4">People</font></a>
-            </Navigation>
+      <div className="header">
+          <Header className = "header-color" transparent title={<span style={{ color: '#201115' }}></span>}>
+          <a href="/">
+            <img
+              src={Logo} 
+              alt="logo1"
+              className="scribr-logo"
+            />
+          </a>
+        
+          <div className ="navigationbar">
+              <Navigation>
+                  <a href="/about" style={{color: 'black'}}><font size="4">About</font></a>
+                  <a href="/magic" style={{color: 'black'}}><font size="4">Transcribe</font></a>
+                  <a href="/people" style={{color: 'black'}}><font size="4">Team</font></a>
+              </Navigation>
+          </div>
+          </Header>
         </div>
-        </Header>
-        <hr/>
         <Content>
           <div className="page-content"/>
           <Main />
